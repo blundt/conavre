47d8867ea8583aea39afb52f50f652e0527053b9
Brian LT <brian.lund.t@outlook.com>
2024-11-05 13:08:23 -0300

    en index se agrega funcion de galeria en la seccion de proyectos y se corrige el tamaño de los cuadros en servicios, se agregan imagenes correspondiente a la galeria 3 que son de los refugios de nieve, se agrega la funcion en el main.js para colocar 9 imagenes por proyecto, ademas se trabaja el css con los estilos correspondientes en styles.css para estos mismos fines

--- js/main.js
+++ js/main.js
@@ -6,3 +6,29 @@ function toggleVisibility(id) {
         element.style.display = "none";
     }
 }
+
+// Array de imágenes para cada galería
+const galleryImages = {
+    gallery1: ["img/gallery1-1.jpg", "img/gallery1-2.jpg", "img/gallery1-3.jpg", "img/gallery1-4.jpg", "img/gallery1-5.jpg", "img/gallery1-6.jpg", "img/gallery1-7.jpg", "img/gallery1-8.jpg", "img/gallery1-9.jpg"],
+    gallery2: ["img/gallery2-1.jpg", "img/gallery2-2.jpg", "img/gallery2-3.jpg", "img/gallery2-4.jpg", "img/gallery2-5.jpg", "img/gallery2-6.jpg", "img/gallery2-7.jpg", "img/gallery2-8.jpg", "img/gallery2-9.jpg"],
+    gallery3: ["img/gallery3-1.jpg", "img/gallery3-2.jpg", "img/gallery3-3.jpg", "img/gallery3-4.jpg", "img/gallery3-5.jpg", "img/gallery3-6.jpg", "img/gallery3-7.jpg", "img/gallery3-8.jpg", "img/gallery3-9.jpg"]
+};
+
+// Índice actual de la imagen mostrada
+const currentIndex = {
+    gallery1: 0,
+    gallery2: 0,
+    gallery3: 0
+};
+
+// Función para mostrar la imagen anterior
+function prevImage(galleryId) {
+    currentIndex[galleryId] = (currentIndex[galleryId] - 1 + galleryImages[galleryId].length) % galleryImages[galleryId].length;
+    document.getElementById(galleryId).src = galleryImages[galleryId][currentIndex[galleryId]];
+}
+
+// Función para mostrar la imagen siguiente
+function nextImage(galleryId) {
+    currentIndex[galleryId] = (currentIndex[galleryId] + 1) % galleryImages[galleryId].length;
+    document.getElementById(galleryId).src = galleryImages[galleryId][currentIndex[galleryId]];
+}
