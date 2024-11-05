9d133267287d102a340561941ffa7f2c04c18f6a
Brian LT <brian.lund.t@outlook.com>
2024-11-05 16:14:08 -0300

    se agrega y cambian imagenes de gallery1 ademas se agrega un fabicon y se coloca el link en los html para el fabicon

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function toggleVisibility(id) {
 
 // Array de imágenes para cada galería
 const galleryImages = {
-    gallery1: ["img/gallery1-1.jpg", "img/gallery1-2.jpg", "img/gallery1-3.jpg", "img/gallery1-4.jpg", "img/gallery1-5.jpg", "img/gallery1-6.jpg", "img/gallery1-7.jpg", "img/gallery1-8.jpg", "img/gallery1-9.jpg", "img/gallery1-10.jpg", "img/gallery1-11.jpg", "img/gallery1-12.jpg", "img/gallery1-13.jpg"],
+    gallery1: ["img/gallery1-1.jpg", "img/gallery1-2.jpg", "img/gallery1-3.jpg", "img/gallery1-4.jpg", "img/gallery1-5.jpg", "img/gallery1-6.jpg", "img/gallery1-7.jpg", "img/gallery1-8.jpg", "img/gallery1-9.jpg", "img/gallery1-10.jpg", "img/gallery1-11.jpg", "img/gallery1-12.jpg", "img/gallery1-13.jpg", "img/gallery1-14.jpg"],
     gallery2: ["img/gallery2-1.jpg", "img/gallery2-2.jpg", "img/gallery2-3.jpg", "img/gallery2-4.jpg", "img/gallery2-5.jpg", "img/gallery2-6.jpg", "img/gallery2-7.jpg", "img/gallery2-8.jpg", "img/gallery2-9.jpg"],
     gallery3: ["img/gallery3-1.jpg", "img/gallery3-2.jpg", "img/gallery3-3.jpg", "img/gallery3-4.jpg", "img/gallery3-5.jpg", "img/gallery3-6.jpg", "img/gallery3-7.jpg", "img/gallery3-8.jpg", "img/gallery3-9.jpg"]
 };
