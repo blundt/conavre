d5ea6f822760a7abb9eb477acda7f28e723db5bf
Brian LT <brian.lund.t@outlook.com>
2024-11-08 15:09:29 -0300

    se modifica el contact.html su css y el php del formulario, ademas se agrega un boton en services para dirigirse a contact, y finalmente en about se modifica un poco el texto y se elimina la parte nuestra historia. Con esta version la pagina comienza a funcionar online con restriccion de acceso en products.html por falta de catalogos

--- php/enviar_formulario.php
+++ php/enviar_formulario.php
@@ -3,19 +3,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recoge y limpia los datos del formulario
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
+    $title_text = htmlspecialchars($_POST['title_text']);
     $message = htmlspecialchars($_POST['message']);
+    $company = htmlspecialchars($_POST['company']);
 
     // Dirección de correo electrónico a la que se enviará el mensaje
     $to = "contacto@conavre.com";
-    $subject = "Nuevo mensaje del formulario de contacto";
-    $body = "Nombre: $name\nEmail: $email\nMensaje:\n$message";
+    $subject = $title_text; // El asunto será el título del texto
+    $body = "Nombre: $name\nEmpresa: $company\nEmail: $email\n\nMensaje:\n$message\n\n---\nNombre: $name\nEmpresa: $company\nCorreo Electrónico: $email";
 
     // Cabeceras de correo
     $headers = "From: $email";
 
     // Enviar el correo
     if (mail($to, $subject, $body, $headers)) {
-        echo "Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.";
+        echo "Gracias por tu mensaje. Nos pondremos en contacto contigo pronto. Serás redirigido a la página principal en 5 segundos.";
+        header("refresh:5;url=../index.html");
     } else {
         echo "Lo sentimos, ha ocurrido un error. Por favor, inténtalo de nuevo.";
     }
